2c31323280d8293a3fd34c1415e02d6f037a9c22
md.rahman <md.rahman@thoughtworks.com>
2020-04-14 20:12:17 +0530

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,21 @@
+
+import React from "react";
+// es6 module
+
+// function declration
+// function Header() {
+//     return <h1>Header Component</h1>
+// }
+
+// function expression
+// const Header = function () {
+//     return <h1>Header Component</h1>
+// }
+
+const Header = () => <h1>Header Component</h1>
+
+ const Footer = () => <div>&copy;2020 products.com</div>
+
+export { Footer };
+
+export default Header;
