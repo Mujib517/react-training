e6bcfbde35ee74e86df63b23d1a01cec76cf9353
md.rahman <mujib517@gmail.com>
2020-04-28 20:19:52 +0530

    Dockerize app

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ app.listen(PORT, () => console.log("Server is running..."));
 app.get("/health", (req, res) => res.status(200).json({ status: "Up" }));
 
 
+// React app
+// React version: 16.19
+// Node Version: 14.0
+// Linux : 
+
+// 100GB 
+// 4 25GB
+// Environment
+// Linux, Node, Nginx
+// containerizing
+
+
