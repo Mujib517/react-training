b946cb3f0b8455dde44ab8c1a6f41a18c907365d
md.rahman <mujib517@gmail.com>
2020-04-28 19:56:38 +0530

    Change Port

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 app.use(express.static("build"));
 app.use(express.static(__dirname + '/build'));
 
-app.listen(3000, () => console.log("Server is running..."));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Server is running..."));
 
 app.get("/health", (req, res) => res.status(200).json({ status: "Up" }));
 
