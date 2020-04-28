b427516f129c64836a7b19d8ae7f9937d4a9eb35
md.rahman <mujib517@gmail.com>
2020-04-28 19:34:54 +0530

    Context api

--- src/Home.js
+++ src/Home.js
@@ -35,6 +35,37 @@ componentDidCatch()
 const Home = () => <>
     <Name name={10} />
     <h1>Home Component</h1>
+    <Parent />
 </>
+
+// context api
+const NameContext = React.createContext();
+
+const Parent = () => <>
+    <h4>Parent</h4>
+    <NameContext.Provider value={100}>
+        <Level1 />
+    </NameContext.Provider>
+</>
+
+const Level1 = () => <>
+    <h4>Level 1</h4>
+    <Level2 />
+</>
+
+
+const Level2 = () => <>
+    <h4>Level 2</h4>
+    <Level3 />
+</>
+
+
+const Level3 = () => <>
+    <h4>Level 3</h4>
+    <NameContext.Consumer>
+        {value => <h4>{value}</h4>}
+    </NameContext.Consumer>
+</>
+
 
 export default Home;
